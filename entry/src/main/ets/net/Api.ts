339f409bcda9f11b84bccf8491a02699a4366d63
jtl <604872657@qq.com>
2024-03-11 03:26:26 +0800

    搜索页面 cookie解决

--- entry/src/main/ets/net/Api.ts
+++ entry/src/main/ets/net/Api.ts
@@ -1,27 +1,11 @@
-import axios, { AxiosError, AxiosInstance, AxiosResponse } from '@ohos/axios'
-import url from '@ohos.url';
+import axios, {AxiosInstance} from '@ohos/axios'
 import HomeBean from '../bean/HomeBean';
 import { HomeSearchBean } from '../bean/HomeSearchBean';
 import { PlayVideoBean } from '../bean/PlayVideoBean';
 import { SearchHotBean } from '../bean/SearchHotBean';
-import { SearchDetailsBean } from '../bean/SearchDetailsBean';
+import { SearchDetailsBean, SearchDetailsItemBean } from '../bean/SearchDetailsBean';
 
 class Api {
-  cookie = "buvid3=F990C62D-8570-E810-1D2A-42B652D1376F29722infoc; b_nut=1699589929; i-wanna-go-back=-1; b_ut=7; _uuid=C23E3C8B-4A3F-3BEC-4FEE-6C7692E8178330018infoc; enable_web_push=DISABLE; buvid4=A26F40F9-7FBF-43AA-5BD9-80F4924FCBFD30208-023111012-KxfNF8XJX%2FbkT0eszmLSXw%3D%3D; rpdid=|(k|kmJluuJR0J'uYmml)mmJk; DedeUserID=68406011; DedeUserID__ckMd5=ecb0ad5cd0bcd251; header_theme_version=CLOSE; is-2022-channel=1; buvid_fp_plain=undefined; hit-dyn-v2=1; LIVE_BUVID=AUTO3317015948042453; CURRENT_BLACKGAP=0; bsource_origin=baidu_aladdin; share_source_origin=COPY; CURRENT_QUALITY=120; bsource=search_baidu; FEED_LIVE_VERSION=V8; home_feed_column=5; browser_resolution=1920-911; fingerprint=278b352fcb69f1c769fe12178cc9fe1b; CURRENT_FNVAL=4048; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTAwNjgxNDIsImlhdCI6MTcwOTgwODg4MiwicGx0IjotMX0.w-5IndBc-Xpv3tlbKr3KykXZx_Sb_xQUsXTXAjOyfXE; bili_ticket_expires=1710068082; SESSDATA=d333e056%2C1725362908%2Ce386a%2A31CjDLM2XkYZ9e0aFjsWnnpjc6U58h5iow8_gZjoFFmJq_a0fcnl7eZE4JI1APflV4EbUSVjNmTDRDOXE3LUdzRUFOTjUtWkJpN1ZHWjU3S3BMNEswaGQyT3JoYVdETzBqTVRtbDQtdVdQeC1KdXJlczYybFZwVHNVNXROb3owY29qam1LVWJXeTBnIIEC; bili_jct=2c7974516be840558eca0adc78db326f; sid=5qt75f39; buvid_fp=278b352fcb69f1c769fe12178cc9fe1b; bp_article_offset_68406011=906886996051361810; bp_video_offset_68406011=906890200062361608; PVID=3; b_lsid=3EFE9261_18E24977114";
-
-  // headers = new Map([
-  //   ["User-Agent", 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'],
-  //   ["Connection", "keep-alive"],
-  //   ["Referer", "https://www.bilibili.com"],
-  //   ["Cookie",this.cookie]
-  // ]);
-  cookieHeaders: {
-    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
-    "Connection": "keep-alive",
-    "Referer": "https://www.bilibili.com",
-    "Cookie","buvid3=F990C62D-8570-E810-1D2A-42B652D1376F29722infoc; b_nut=1699589929; i-wanna-go-back=-1; b_ut=7; _uuid=C23E3C8B-4A3F-3BEC-4FEE-6C7692E8178330018infoc; enable_web_push=DISABLE; buvid4=A26F40F9-7FBF-43AA-5BD9-80F4924FCBFD30208-023111012-KxfNF8XJX%2FbkT0eszmLSXw%3D%3D; rpdid=|(k|kmJluuJR0J'uYmml)mmJk; DedeUserID=68406011; DedeUserID__ckMd5=ecb0ad5cd0bcd251; header_theme_version=CLOSE; is-2022-channel=1; buvid_fp_plain=undefined; hit-dyn-v2=1; LIVE_BUVID=AUTO3317015948042453; CURRENT_BLACKGAP=0; bsource_origin=baidu_aladdin; share_source_origin=COPY; CURRENT_QUALITY=120; bsource=search_baidu; FEED_LIVE_VERSION=V8; home_feed_column=5; browser_resolution=1920-911; fingerprint=278b352fcb69f1c769fe12178cc9fe1b; CURRENT_FNVAL=4048; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTAwNjgxNDIsImlhdCI6MTcwOTgwODg4MiwicGx0IjotMX0.w-5IndBc-Xpv3tlbKr3KykXZx_Sb_xQUsXTXAjOyfXE; bili_ticket_expires=1710068082; SESSDATA=d333e056%2C1725362908%2Ce386a%2A31CjDLM2XkYZ9e0aFjsWnnpjc6U58h5iow8_gZjoFFmJq_a0fcnl7eZE4JI1APflV4EbUSVjNmTDRDOXE3LUdzRUFOTjUtWkJpN1ZHWjU3S3BMNEswaGQyT3JoYVdETzBqTVRtbDQtdVdQeC1KdXJlczYybFZwVHNVNXROb3owY29qam1LVWJXeTBnIIEC; bili_jct=2c7974516be840558eca0adc78db326f; sid=5qt75f39; buvid_fp=278b352fcb69f1c769fe12178cc9fe1b; bp_article_offset_68406011=906886996051361810; bp_video_offset_68406011=906890200062361608; PVID=3; b_lsid=3EFE9261_18E24977114";
-  }
-
   baseURl = 'https://api.bilibili.com/x'
   defaultURL = 'https://api.bilibili.com/x/web-interface/index/top/feed/rcmd?ps=12&fresh_idx=1&feed_version=undefined'
   searchURl = 'https://api.bilibili.com/x/web-interface/search/default'
@@ -36,10 +20,19 @@ class Api {
     timeout: 10000,
     headers: {
       "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
-      "Connection": "keep-alive",
       "Referer": "https://www.bilibili.com",
     }
-  });
+  })
+
+  instanceCookie = axios.create({
+    baseURL: this.baseURl,
+    timeout: 10000,
+    headers: {
+      "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+      "Cookie": "buvid3=F990C62D-8570-E810-1D2A-42B652D1376F29722infoc; b_nut=1699589929; i-wanna-go-back=-1; b_ut=7; _uuid=C23E3C8B-4A3F-3BEC-4FEE-6C7692E8178330018infoc; enable_web_push=DISABLE; buvid4=A26F40F9-7FBF-43AA-5BD9-80F4924FCBFD30208-023111012-KxfNF8XJX%2FbkT0eszmLSXw%3D%3D; rpdid=|(k|kmJluuJR0J'uYmml)mmJk; header_theme_version=CLOSE; is-2022-channel=1; buvid_fp_plain=undefined; hit-dyn-v2=1; LIVE_BUVID=AUTO3317015948042453; CURRENT_BLACKGAP=0; bsource_origin=baidu_aladdin; share_source_origin=COPY; CURRENT_QUALITY=120; bsource=search_baidu; fingerprint=278b352fcb69f1c769fe12178cc9fe1b; CURRENT_FNVAL=4048; buvid_fp=278b352fcb69f1c769fe12178cc9fe1b; bp_article_offset_68406011=906886996051361810; PVID=3; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTAzMzAxOTAsImlhdCI6MTcxMDA3MDkzMCwicGx0IjotMX0.VLgbgRpLy7msd_B6vcL0Lv9STAbYZ-tq5FrmyowvDaY; bili_ticket_expires=1710330130; SESSDATA=75afeffc%2C1725623018%2Cf6519%2A32CjDLqHUKV3R3Hylels4DdBS6jn524Y7iv_VRiHwIQ1t8PSnYsujZho3mQmGWb0tK9qgSVklDbUM5ZWp5ZHlNcU1Hd1EySEkwN2liWUY5cUx0ODZwdzQ4TVRxS2dpbDhkRTh5eE5uR1YyZ2hlMTN1dUtyZTdkZ1dqb0x5Zk0tRDVQMWRZNGFSSzR3IIEC; bili_jct=41a765da91704002b1b39082b08b43f6; DedeUserID=68406011; DedeUserID__ckMd5=ecb0ad5cd0bcd251; sid=4o8c3o70; FEED_LIVE_VERSION=V8; bp_video_offset_68406011=907237121746534418; b_lsid=1C101875D_18E291813FF; home_feed_column=4; browser_resolution=622-911",
+      "Referer": "https://www.bilibili.com"
+    }
+  })
 
   getApi(): AxiosInstance {
     return this.instance
@@ -82,9 +75,7 @@ class Api {
   getPlayVideo(bvid: string, cid: number): Promise<PlayVideoBean> {
     return new Promise((resolve, reject) => {
       //let url = `${this.playURL}?bvid=${bvid}&cid=${cid}&qn=112`
-      this.instance.get(`/player/playurl?bvid=${bvid}&cid=${cid}`, {
-        headers: this.cookieHeaders
-      }).then(resp => {
+      this.instance.get(`/player/playurl?bvid=${bvid}&cid=${cid}`).then(resp => {
         if (resp.status == axios.HttpStatusCode.Ok) {
           resolve(resp.data.data)
         } else {
@@ -114,10 +105,29 @@ class Api {
     })
   }
 
+  getSearchAllDetails(video?: string, keyword?: string, page: number = 0): Promise<SearchDetailsBean> {
+    return new Promise((resolve, reject) => {
+      //https://api.bilibili.com/x/web-interface/search/all/v2
+      let url = `/web-interface/search/type?search_type=video&keyword=${keyword}&page=${page}`
+      // this.instance.defaults.headers["cookie"] = this.cookie
+      this.instanceCookie.get(url).then(resp => {
+        if (resp.status == axios.HttpStatusCode.Ok) {
+          resolve(resp.data.data)
+        } else {
+          reject("请求失败：" + resp.status)
+          console.log("jtl:" + resp.status)
+        }
+        console.log("jtl:getSearchDetails:" + resp.status + "---" + JSON.stringify(resp.data))
+      }).catch(error => {
+        console.log("jtl:" + error)
+        reject("请求失败：" + error)
+      })
+    })
+  }
   getSearchDetails(video?: string, keyword?: string, page: number = 0): Promise<SearchDetailsBean> {
     return new Promise((resolve, reject) => {
       let url = `/web-interface/search/type?search_type=video&keyword=${keyword}&page=${page}`
-      this.instance.get(url, { headers: this.cookieHeaders }).then(resp => {
+      this.instanceCookie.get(url).then(resp => {
         if (resp.status == axios.HttpStatusCode.Ok) {
           resolve(resp.data.data)
         } else {
---
+++ entry/src/main/ets/net/ApiConstants.ts
@@ -0,0 +1,6 @@
+// 获取首页视频推荐列表（web端）
+let homeRecommendVideoList = "https://api.bilibili.com/x/web-interface/index/top/rcmd"
+// 短视频列表 Get
+let shortVideoList = "https://app.bilibili.com/x/v2/feed/index"
+// 单个视频 推荐列表 bvid
+let singleVideoRecommendList =" https://api.bilibili.com/x/web-interface/archive/related?bvid=BV1xx411c7m9"
