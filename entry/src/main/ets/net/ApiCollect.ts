ff7732579a3405a2594a9375d4f2dce1b8f2a45d
jtl <604872657@qq.com>
2024-03-30 15:42:31 +0800

    UI 布局小修改，底部slider条

---
+++ entry/src/main/ets/net/ApiCollect.ts
@@ -0,0 +1,7 @@
+// https://api.bilibili.com/x/v3/fav/folder/created/list-all?up_mid=68406011&type=0
+// 获取所有收藏夹数据(type = 0为全部稿件 type=2为视频稿件)
+
+// 获取收藏夹内容明细列表
+//https://api.bilibili.com/x/v3/fav/resource/list?media_id=1052622027&platform=web&pn=1&ps=5&keyword=""&order = mtime
+// order 按收藏时间:mtime,按播放量: view,按投稿时间：pubtime
+// media_id为收藏夹id。keyword可以没有。
---
+++ entry/src/main/ets/net/ApiHistory.ts
@@ -0,0 +1,5 @@
+// https://api.bilibili.com/x/web-interface/history/cursor
+// 查询历史记录 新版
+
+// https://api.bilibili.com/x/v2/history?pn=1&ps=20
+// 查询历史记录 旧版 pn为页数，ps为每页的数量
