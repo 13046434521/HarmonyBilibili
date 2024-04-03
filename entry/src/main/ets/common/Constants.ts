fa57cebf6482844f496d240607a99f57d40feb66
jtl <604872657@qq.com>
2024-04-03 22:13:01 +0800

    Mine的web链接以及整体ui更新。部分无图标。可以跳转使用

--- entry/src/main/ets/common/Constants.ts
+++ entry/src/main/ets/common/Constants.ts
@@ -22,9 +22,12 @@ class Constants{
   /*------------------------------------------- AppStorage --------------------------------------------*/
   Login_Data:string = 'Bilibili_Login'
   Home_Data:string = 'Bilibili_Home'
+
   getCookie():string{
     return StorageManager.getCookie()
   }
+  /*------------------------------------------- WEB --------------------------------------------*/
+  PHONE_USER_AGENT='Mozilla/5.0 (Linux; Android 8.0.0; SM-G955U Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36'
 }
 
 const constants :Constants = new Constants()
